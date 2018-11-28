bf39445d976883c3c0f2cc000458970a209c2502
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2018-11-28 15:48:56 -0800

    add example for multivalue feature

--- example/js/client-controls.jsx
+++ example/js/client-controls.jsx
@@ -2,22 +2,47 @@ import React from 'react';
 import client from './client';
 
 const flags = ['one', 'two', 'three'];
+const multivalueFlagName = 'multivalueFlag';
+const multivalueFlagValues = ['one', 'two', 'three'];
 
 const ClientControls = () => (
-  <form>
-    {flags.map(flag => (
-      <div key={flag}>
-        <label htmlFor={flag}>
-          <input
-            id={flag}
-            type="checkbox"
-            onChange={e => client.setValue(flag, e.target.checked)}
-          />
-          {flag}
-        </label>
-      </div>
-    ))}
-  </form>
+  <div>
+    <section>
+      <h3>Boolean flags</h3>
+      <form>
+        {flags.map(flag => (
+          <div key={flag}>
+            <label htmlFor={flag}>
+              <input
+                id={flag}
+                type="checkbox"
+                onChange={e => client.setValue(flag, e.target.checked)}
+              />
+              {flag}
+            </label>
+          </div>
+        ))}
+      </form>
+    </section>
+    <section>
+      <h3>Multi-value flag</h3>
+      <form>
+        {multivalueFlagValues.map(flag => (
+          <div key={flag}>
+            <label htmlFor={flag}>
+              <input
+                id={flag}
+                type="radio"
+                name={multivalueFlagName}
+                onChange={() => client.setValue(multivalueFlagName, flag)}
+              />
+              {flag}
+            </label>
+          </div>
+        ))}
+      </form>
+    </section>
+  </div>
 );
 
 export default ClientControls;
--- example/js/feature-display.jsx
+++ example/js/feature-display.jsx
@@ -3,21 +3,44 @@ import { Feature, Option } from '../../build/module';
 
 const FeatureDisplay = () => (
   <section>
-    <Feature name="one">
-      <Option value={true}>
-        <span>Feature One</span>
-      </Option>
-    </Feature>
-    <Feature name="two">
-      <Option value={true}>
-        <span>Feature Two</span>
-      </Option>
-    </Feature>
-    <Feature name="three">
-      <Option value={true}>
-        <span>Feature Three</span>
-      </Option>
-    </Feature>
+    <ul>
+      <Feature name="one">
+        <Option value>
+          <li>
+            <span>Feature One</span>
+          </li>
+        </Option>
+      </Feature>
+
+      <Feature name="two">
+        <Option value>
+          <li>
+            <span>Feature Two</span>
+          </li>
+        </Option>
+      </Feature>
+
+      <Feature name="three">
+        <Option value>
+          <li>
+            <span>Feature Three</span>
+          </li>
+        </Option>
+      </Feature>
+    </ul>
+    <ul>
+      <Feature name="multivalueFlag">
+        <Option value="one">
+          <span>Multivalue feature one</span>
+        </Option>
+        <Option value="two">
+          <span>Multivalue feature two</span>
+        </Option>
+        <Option value="three">
+          <span>Multivalue feature three</span>
+        </Option>
+      </Feature>
+    </ul>
   </section>
 );
 
