a93d826fc220503faf17891f2e5b554d79c434fb
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2018-11-29 19:57:29 -0800

    add provider test

--- __tests__/provider.test.js
+++ __tests__/provider.test.js
@@ -1,8 +1,42 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Provider from '../src/components/provider';
+import featureManagementClientSahpe from '../src/client/feature-management-client-shape';
 
-test('should pass', () => {
-  const wrapper = shallow(<Provider />);
-  expect(wrapper).toBeDefined();
+const client = {
+  subscribe: jest.fn(),
+  getFeatureFlagValue: jest.fn()
+};
+
+class ClientConsumer extends React.Component {
+  componentWillMount() {
+    const { featureManagementClient } = this.context;
+    this.setState({ featureManagementClient });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {Object.keys(this.state.featureManagementClient).map(propertyName => (
+          <div key={propertyName}>{propertyName}</div>
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+
+ClientConsumer.contextTypes = {
+  featureManagementClient: featureManagementClientSahpe
+};
+
+test('should render Providers children and pass client', () => {
+  const wrapper = mount(
+    <Provider client={client}>
+      <ClientConsumer />
+    </Provider>
+  );
+
+  expect(wrapper.contains(<ClientConsumer />)).toEqual(true);
+  expect(wrapper.find(<div>subscribe</div>)).toBeDefined();
+  expect(wrapper.find(<div>getFeatureFlagValue</div>)).toBeDefined();
 });
